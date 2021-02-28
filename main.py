e9d17590318924623c88d2db32df3103a8ebcdb7
Adam Goddard <adam_goddard1@outlook.com>
2021-02-28 16:56:26 +0000

    Style: improve comment clarity

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import requests
 import os
 from twilio.rest import Client
 
-# The latitude and longitude of the location that weather should be checked at
+# The latitude and longitude of the location where weather should be monitored
 LOCATION_LAT = '52.520008'
 LOCATION_LONG = '13.404954'
 
-# API keys and Twilio account details (see Github ReadMe)
+# API keys and Twilio account details (see readme)
 OPEN_WEATHER_API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
 TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-
-# Phone number for the Twilio account and the phone number of the person you
-# would like an SMS to be sent to
 TWILIO_ACCOUNT_PHONE_NUMBER = os.getenv('TWILIO_ACCOUNT_PHONE_NUMBER')
+
+# Phone number which SMS rain warnings should be sent to
 RECIPIENT_PHONE_NUMBER = os.getenv('RECIPIENT_PHONE_NUMBER')
 
 OPEN_WEATHER_ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall'
@@ -24,8 +23,7 @@ parameters = {
     'exclude': 'current,minutely,daily'
 }
 
-# Get weather forecast from the Open weather API and check weather codes to see
-# if rain is likely
+# Get weather forecast for the next 12 hours from the Open weather API
 response = requests.get(OPEN_WEATHER_ENDPOINT, params=parameters)
 response.raise_for_status()
 weather_data = response.json()
@@ -34,6 +32,7 @@ hourly_weather_codes = [
     hour['weather'][0]['id'] for hour in weather_data['hourly'][:12]
 ]
 
+# Check weather codes to see if rain is likely
 will_rain = False
 for code in hourly_weather_codes:
     if int(code) < 700:
@@ -41,7 +40,7 @@ for code in hourly_weather_codes:
         will_rain = True
         break
 
-# Send an SMS to the user if rain is likely to occur
+# Send an SMS to the user if rain is likely
 if will_rain:
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     message = client.messages.create(
